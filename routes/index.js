99286c4e50403b817d6a8fc3f8c84dce71e4a9e3
Yordan <yordan@wego.com>
2014-04-21 22:34:38 +0700

    init

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/list', function(req, res) {
+  res.render('listing', { title: 'Movie listing' });
+});
+
+module.exports = router;
